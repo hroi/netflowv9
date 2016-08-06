fa2f347ebe3e012e3c2fda171722b7ca6bb7833f
Hroi Sigurdsson <hroi@asdf.dk>
2016-08-06 21:01:32 +0200

    Print flow timestamp and duration. Minor refactoring.

--- src/bin/play.rs
+++ src/bin/play.rs
@@ -1,5 +1,3 @@
-extern crate netflowv9;
-use netflowv9::*;
 use std::collections::HashMap;
 use std::env;
 use std::io::prelude::*;
@@ -8,31 +6,53 @@ use std::mem;
 use std::net::{Ipv4Addr, IpAddr};
 use std::process;
 
+extern crate netflowv9;
+use netflowv9::*;
+
 extern crate flate2;
 use flate2::read::ZlibDecoder;
+
+extern crate time;
 
 type TemplateId = u16;
 type SourceId = u32;
 
-fn print_record<W>(w: &mut W, rec: &Record) where W: Write {
-    let proto = match rec.protocol_identifier()  {
-        Some(1) => "ICMP",
-        Some(6) => "TCP",
+fn print_record<W>(w: &mut W, header: &Header, rec: &Record) where W: Write {
+    let protocol_name = match rec.protocol_identifier()  {
+        Some(1)  => "ICMP",
+        Some(6)  => "TCP",
         Some(17) => "UDP",
         Some(47) => "GRE",
         Some(50) => "ESP",
-        Some(_) => "unk",
+        Some(_)  => "unk",
         _ => panic!()
     };
-    let source_ip = rec.source_ipv4_address().map(|ip| IpAddr::V4(ip.into()))
-        .or(rec.source_ipv6_address().map(|ip| IpAddr::V6(ip.into()))).unwrap();
-    let destination_ip = rec.destination_ipv4_address().map(|ip| IpAddr::V4(ip.into()))
-        .or(rec.destination_ipv6_address().map(|ip| IpAddr::V6(ip.into()))).unwrap();
+    let source_ip = rec.source_ipv4_address()
+        .map(|ip| IpAddr::V4(ip.into()))
+        .or(rec.source_ipv6_address()
+            .map(|ip| IpAddr::V6(ip.into()))).unwrap();
+    let destination_ip = rec.destination_ipv4_address()
+        .map(|ip| IpAddr::V4(ip.into()))
+        .or(rec.destination_ipv6_address()
+            .map(|ip| IpAddr::V6(ip.into()))).unwrap();
 
-    writeln!(w, "{:6} bytes {:6} pkt {:6} {}:{} -> {}:{}",
+    let packet_time = header.seconds() as u64 * 1000;
+    let sys_uptime = header.sys_uptime() as u64;
+    let boot_time = packet_time - sys_uptime;
+    let flow_start = boot_time + rec.flow_start_sys_uptime().unwrap_or(0) as u64;
+    let flow_end = boot_time + rec.flow_end_sys_uptime().unwrap_or(0) as u64;
+
+    let duration = flow_end - flow_start;
+
+    let start = time::at_utc(time::Timespec{ sec: (flow_start / 1000) as i64, nsec: 0});
+    //let end = time::at_utc(time::Timespec{ sec: (flow_end / 1000) as i64, nsec: 0});
+
+    writeln!(w, "{} {:>5.1}s {:6} bytes {:5} pkts {:6} {}:{} -> {}:{}",
+             start.rfc3339(),
+             (duration as f64) / 1000.0,
              rec.octet_delta_count().unwrap_or(0),
              rec.packet_delta_count().unwrap_or(0),
-             proto,
+             protocol_name,
              source_ip,
              rec.source_transport_port().unwrap_or(0),
              destination_ip,
@@ -44,7 +64,7 @@ fn main() {
     let stdin = io::stdin();
     let stdout = io::stdout();
     let mut stdinl = ZlibDecoder::new(stdin.lock());
-    let mut stdoutl = stdout.lock();
+    let mut out = stdout.lock();
     let mut buf = [0u8; 2048];
     let mut seq_nums: HashMap<SourceId, u32> = HashMap::new();
     let mut extractors: HashMap<TemplateId, FlowRecordExtractor> = HashMap::new();
@@ -72,7 +92,7 @@ fn main() {
             Ok(_) => (),
             Err(err) => {
                 if err.kind() != std::io::ErrorKind::UnexpectedEof {
-                    writeln!(stdoutl, "Error: {}", err).unwrap();
+                    writeln!(out, "Error: {}", err).unwrap();
                     exit_status = -1;
                 }
                 break;
@@ -84,13 +104,13 @@ fn main() {
         };
 
         if packet_length > buf.len() {
-            writeln!(stdoutl, "invalid packet length: {}", packet_length).unwrap();
+            writeln!(out, "invalid packet length: {}", packet_length).unwrap();
             exit_status = -1;
             break;
         }
 
         if stdinl.read_exact(&mut buf[..packet_length]).is_err() {
-            writeln!(stdoutl, "buf read err").unwrap();
+            writeln!(out, "buf read err").unwrap();
             break;
         }
 
@@ -126,14 +146,16 @@ fn main() {
                     FlowSet::DataRecords(ref data) => {
                         if let Some(extractor) = extractors.get(&data.template_id) {
                             for record in extractor.records(data) {
-                                if let (Some(src_ip), Some(dst_ip)) = (record.source_ipv4_address(), record.destination_ipv4_address()) {
-                                    for ip in interesting_ips.iter() {
-                                        if src_ip == *ip || dst_ip == *ip {
-                                            // writeln!(stdoutl, "{:?}", record).unwrap();
-                                            print_record(&mut stdoutl, &record);
+                                if let (Some(src_ip), Some(dst_ip)) =
+                                    (record.source_ipv4_address(), record.destination_ipv4_address()) {
+                                    for ip in &interesting_ips {
+                                        if *ip == src_ip || *ip == dst_ip {
+                                            // writeln!(out, "{:?}", record).unwrap();
+                                            print_record(&mut out, &header, &record);
                                         }
                                     }
                                 }
+
                                 nflows += 1;
                                 noctets += record.octet_delta_count().unwrap_or(0);
                                 npackets += record.packet_delta_count().unwrap_or(0);
@@ -146,18 +168,17 @@ fn main() {
                     _ => ()
                 }
             }
-
         } else {
-            writeln!(stdoutl, "packet decode err").unwrap();
+            writeln!(out, "packet decode err").unwrap();
             exit_status = -1;
             break;
         }
     }
 
     if interesting_ips.is_empty() {
-        writeln!(stdoutl, "non-increasing sequence numbers: {}, gaps: {}, lates: {}",
+        writeln!(out, "non-increasing sequence numbers: {}, gaps: {}, lates: {}",
                  nnonincr, ngaps, nlates).unwrap();
-        writeln!(stdoutl, "pdus: {}, skipped flowsets: {}, flows: {}, octets: {}, packets: {}",
+        writeln!(out, "pdus: {}, skipped flowsets: {}, flows: {}, octets: {}, packets: {}",
                  npdus, nskipped, nflows, noctets, npackets).unwrap();
     }
     process::exit(exit_status);
--- src/bin/record.rs
+++ src/bin/record.rs
@@ -1,55 +1,88 @@
-use std::net::{UdpSocket, IpAddr};
 use std::collections::HashMap;
 use std::fmt::Display;
 use std::fs::{File, OpenOptions};
 use std::io::prelude::*;
 use std::io::BufWriter;
 use std::mem;
+use std::net::{UdpSocket, IpAddr};
 use std::time::{Duration, Instant};
 
 extern crate flate2;
 use flate2::write::ZlibEncoder;
 use flate2::Compression;
 
+// This server indiscriminately saves any packet to a compressed, rotated,
+// per-exporter file. No checking or parsing of PDUs is performed.
+
+fn main() {
+    let compression = Compression::Best;
+    let rotate_interval = Duration::from_secs(300);
+    let mut file_map: HashMap<IpAddr, FlowFileWriter<IpAddr, _>> = HashMap::new();
+    let socket = UdpSocket::bind("[::]:4013").unwrap();
+    let mut recv_buf = [0u8; 2048];
+
+    while let Ok((amt, src)) = socket.recv_from(&mut recv_buf[..]) {
+        let mut output = file_map.entry(src.ip()).or_insert_with(|| {
+            println!("New exporter: {}", src.ip());
+            FlowFileWriter::compressed(src.ip(), compression, rotate_interval).unwrap()
+        });
+
+        output.maybe_rotate().unwrap();
+        output.write(&recv_buf[..amt]).unwrap();
+    }
+}
+
 #[derive(Debug)]
 enum Error {
     Io(std::io::Error)
 }
 
-struct FlowFileWriter<D> where D: Display + Copy {
-    filename: D,
+struct FlowFileWriter<D, W> where D: Display + Copy, W: Write {
+    exporter_name: D,
     generation: usize,
+    compression: Compression,
     last_rotation: Instant,
     rotation_interval: Duration,
-    output: BufWriter<ZlibEncoder<File>>,
+    output: W,
 }
 
-impl<D> FlowFileWriter<D> where D: Display + Copy {
+type Output = BufWriter<ZlibEncoder<File>>;
 
-    fn new(filename: D, interval: Duration) -> Result<FlowFileWriter<D>, Error> {
-        Ok(FlowFileWriter{
-            filename: filename,
-            generation: 0,
-            last_rotation: Instant::now(),
-            rotation_interval: interval,
-            output: BufWriter::new(ZlibEncoder::new(try!(OpenOptions::new()
-                       .create(true) .append(true)
-                       .open(format!("./{}.{:4}.z", filename, 0))
-                       .map_err(|err| { Error::Io(err) })), Compression::Best))})
+impl<D> FlowFileWriter<D, Output> where D: Display + Copy {
+
+    fn compressed(exporter_name: D, compression: Compression, interval: Duration)
+           -> Result<FlowFileWriter<D, Output>, Error> {
+        Ok(
+            FlowFileWriter {
+                exporter_name: exporter_name,
+                generation: 0,
+                compression: compression,
+                last_rotation: Instant::now(),
+                rotation_interval: interval,
+                output: BufWriter::new(
+                    ZlibEncoder::new(
+                        try!(OpenOptions::new()
+                             .create(true)
+                             .append(true)
+                             .open(format!("./{}.{:04}.z", exporter_name, 0))
+                             .map_err(|err| { Error::Io(err) })), compression))
+            }
+        )
     }
 
     fn rotate(&mut self) -> Result<(), Error> {
         self.generation += 1;
         self.output = BufWriter::new(ZlibEncoder::new(
             try!(OpenOptions::new()
-                 .create(true) .append(true)
-                 .open(format!("./{}.{:4}.z", self.filename, self.generation))
-                 .map_err(|err| { Error::Io(err) })), Compression::Best));
+                 .create(true)
+                 .append(true)
+                 .open(format!("./{}.{:04}.z", self.exporter_name, self.generation))
+                 .map_err(|err| { Error::Io(err) })), self.compression));
         self.last_rotation = Instant::now();
         Ok(())
     }
 
-    fn maybe_rotate(&mut self) -> Result<(), Error>{
+    fn maybe_rotate(&mut self) -> Result<(), Error> {
         if self.last_rotation.elapsed() > self.rotation_interval {
             self.rotate()
         } else {
@@ -58,7 +91,7 @@ impl<D> FlowFileWriter<D> where D: Display + Copy {
     }
 }
 
-impl<D> Write for FlowFileWriter<D> where D: Display + Copy {
+impl<D,W> Write for FlowFileWriter<D, W> where D: Display + Copy, W: Write {
 
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         let length: [u8; 2] = unsafe { mem::transmute((buf.len() as u16).to_be()) };
@@ -70,22 +103,3 @@ impl<D> Write for FlowFileWriter<D> where D: Display + Copy {
         self.output.flush()
     }
 }
-
-fn main() {
-
-    let mut file_map: HashMap<IpAddr, FlowFileWriter<IpAddr>> = HashMap::new();
-    let port = 4013;
-    let socket = UdpSocket::bind(("::".parse::<IpAddr>().unwrap(), port)).unwrap();
-    let mut recv_buf = [0u8; 2048];
-
-    while let Ok((amt, src)) = socket.recv_from(&mut recv_buf[..]) {
-        let mut output = file_map.entry(src.ip()).or_insert_with(|| {
-            println!("New exporter: {}", src.ip());
-            FlowFileWriter::new(src.ip(), Duration::from_secs(300)).unwrap()
-        });
-
-        output.maybe_rotate().unwrap();
-        output.write(&recv_buf[..amt]).unwrap();
-    }
-}
-
